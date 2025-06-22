5a500538cbd105c736b1e93a85135cf485f95d0d
Nknky09 <ks.shido@gmail.com>
2025-06-22 15:30:55 -0400

    v3.42

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import { signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from "@/db/prisma";
+import { formatError } from "../utils";
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
@@ -44,11 +45,10 @@ export async function SignUpUser(prevState: unknown, formData: FormData) {
       confirmPassword: formData.get("confirmPassword"),
     });
 
+    const plainPassword = user.password;
     user.password = hashSync(user.password, 10);
 
-    const plainPassword = user.password;
-
-    prisma.user.create({
+    await prisma.user.create({
       data: {
         name: user.name,
         email: user.email,
@@ -58,7 +58,7 @@ export async function SignUpUser(prevState: unknown, formData: FormData) {
 
     await signIn("credentials", {
       email: user.email,
-      password: user.password,
+      password: plainPassword,
     });
 
     return { success: true, message: "User registered successfully" };
@@ -66,6 +66,6 @@ export async function SignUpUser(prevState: unknown, formData: FormData) {
     if (isRedirectError(error)) {
       throw error;
     }
-    return { success: false, message: "User was not registered" };
+    return { success: false, message: formatError(error) };
   }
 }
