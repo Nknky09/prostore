1280bef5376a1c88d4e7650b2c3ad20a4d9a8eb0
Nknky09 <ks.shido@gmail.com>
2025-06-24 00:04:29 -0400

    v5.46

---
+++ components/shared/product/add-to-cart.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { CartItem } from "@/types";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { Plus } from "lucide-react";
+import { toast } from "sonner";
+import { addItemToCart } from "@/lib/actions/cart-actions";
+
+const AddToCart = ({ item }: { item: CartItem }) => {
+  const router = useRouter();
+
+  const handleAddToCart = async () => {
+    const res = await addItemToCart(item);
+
+    if (!res.success) {
+      toast.error(res.message || "Failed to add item to cart");
+    } else {
+      toast.success(`${item.name} added to cart`, {
+        action: (
+          <button
+            className="inline-flex items-center gap-2 whitespace-nowrap rounded-md bg-primary px-3 py-1 text-white hover:bg-gray-600"
+            onClick={() => router.push("/cart")}
+          >
+            <Plus className="h-5 w-5" />
+            Go To Cart
+          </button>
+        ),
+      });
+    }
+  };
+
+  return (
+    <Button
+      className="w-full hover:cursor-pointer"
+      type="button"
+      onClick={handleAddToCart}
+    >
+      Add To Cart
+    </Button>
+  );
+};
+
+export default AddToCart;
--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,15 @@
 import { z } from "zod";
-import { insertProductsScheuma } from "@/lib/validator";
+import {
+  insertProductsSchema,
+  insertCartSchema,
+  cartItemSchema,
+} from "@/lib/validator";
 
-export type Product = z.infer<typeof insertProductsScheuma> & {
+export type Product = z.infer<typeof insertProductsSchema> & {
   id: string;
   rating: string;
   createdAt: Date;
 };
+
+export type Cart = z.infer<typeof insertCartSchema>;
+export type CartItem = z.infer<typeof cartItemSchema>;
